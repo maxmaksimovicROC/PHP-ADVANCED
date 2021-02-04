bd03cd715736b48dd67373f530792451f61f648f
max maksimovic <maxmaksimovic@student.rocva.nl>
2021-02-04 17:45:42 +0100

    ad

---
+++ 1-Arrays/taak02/loops-while.php
@@ -0,0 +1,13 @@
+<?php
+$leeftijd = 0;
+
+while($leeftijd < 18){
+    echo 'ik ben ' . $leeftijd . ' jaar oud, dus ik mag nog niet stemmen ';
+    $leeftijd++;
+}
+
+echo " ik ben 18 dus ik heb stemrecht"
+
+
+
+?>
---
+++ 2-Loops/taak02/index.php
@@ -0,0 +1,10 @@
+<?php
+    $age = 0
+    for ( $age; $age < 18; $age++){
+        
+         echo " ik ben ' . $age . ' dus ik mag nog niet stemmen "
+     }
+
+     echo " ik ben 18 dus ik mag stemmen ";
+
+?>
