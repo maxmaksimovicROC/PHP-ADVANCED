2a278f84502983211e36e747702ea932bc1463bb
max maksimovic <maxmaksimovic@student.rocva.nl>
2021-04-08 15:02:09 +0200

    ''

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -1,14 +1,39 @@
+<!DOCTYPE html>
 <html>
     <head>
-        <title>contact</title>
+        <link rel="stylesheet" href="style2.css">
     </head>
     <body>
-        <?php require "extra.php" ?>
-        <form action="advanced-2.php" method="POST">
-            <p>Tabeldikte: <input type="text" name="tabelpx"></p>
-            <p>Padding: <input type="text" name="padding"></p>
-            <?php opties(); ?>
-            <input type="submit" name="submit" value="verstuur">
-        </forum>
+        <div class="wrapper">
+
+            <form name="invoer" action="advanced-2.php" method="POST">   
+            <select name="achtergrond-kleur" id="achtergrond-kleur">
+                <?php
+                $kleuren = array("red", "blue", "green", "black", "brown");
+                    foreach($kleuren as $optie){
+                    echo "<option value=$optie>$optie</option>";};
+                ?>
+            </select>
+            <a>achtergrond kleur</a>        
+             <select name="tekst-kleur" id="tekst-kleur">
+                <?php
+                    $kleuren = array("red", "blue", "green", "black", "brown");
+                        foreach($kleuren as $optie){
+                        echo "<option value=$optie>$optie</option>";};
+                ?>
+            </select>
+            <a>tekst kleur</a>
+
+            <input type="text" name="border">
+            <a>border</a>
+
+            <input type="text" name="padding">
+            <a>padding</a>
+            <input type="submit" name="submit" value="vesrtuur">
+
+
+
+        </div>
     </body>
+
 </html>
--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -1,74 +1,45 @@
+<!DOCTYPE html>
 <html>
     <head>
-        <title>contact</title>
+        <link rel="stylesheet" href="style.css">
         <style>
-        body{
-        background-color: <?php
-        $achtergrond = $_POST["achtergrond"] ?? 'niks';
-        if("red" == $achtergrond){
-            echo "red";
-        } elseif("blue" == $achtergrond){
-            echo "blue";
-        } elseif("green" == $achtergrond){
-            echo "green";
-        } elseif("black" == $achtergrond){
-            echo "black";
-        } elseif("brown" == $achtergrond){
-            echo "brown";
-        }elseif("niks" == $achtergrond){
-            echo "yellow";
-        } ?>;
-        color: <?php
-        $tekstkleur = $_POST["tekstkleur"] ?? 'niks';
-        if("red" == $tekstkleur){
-            echo "red";
-        } elseif("blue" == $tekstkleur){
-            echo "blue";
-        } elseif("green" == $tekstkleur){
-            echo "green";
-        } elseif("black" == $tekstkleur){
-            echo "black";
-        } elseif("brown" == $tekstkleur){
-            echo "brown";
-        }elseif("niks" == $tekstkleur){
-            echo "yellow";
-        } ?>;
-        text-align: <?php $onoff = $_POST["onoff"] ?? 'niks';
-        if("on" == $onoff){
-            echo "center";
-        } elseif("off" == $onoff){
-            echo " ";
-        }elseif("niks" == $onoff){
-            echo " ";
-        } ?>;
-        }
-        table{
-            border: <?php echo $_POST["tabelpx"]; ?>px solid black;
-            padding-left: <?php echo $_POST["padding"]; ?>px;
-        }
+                body {
+                    background-color: <?php echo $_POST["achtergrond-kleur"] ?> ;
+                    color: <?php echo $_POST["tekst-kleur"] ?>;
+                }
+                table,tr,td {
+                    padding: <?php echo $_POST["padding"] ?>px;
+                    border: <?php echo $_POST["border"] ?>px solid black;
+                }
         </style>
     </head>
     <body>
-        <h1>test</h1>
-        <table id="border">
-            <thead>
-              <tr>
-                <th>achtergrond</th>
-                <th>tekstkleur</th>
-                <th>center</th>
-                <th>tabelpx</th>
-                <th>padding</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><?php echo $_POST["achtergrond"]; ?> </td>
-                <td><?php echo $_POST["tekstkleur"]; ?> </td>
-                <td><?php echo $_POST["onoff"]; ?> </td>
-                <td><?php echo $_POST["tabelpx"]; ?> </td>
-                <td><?php echo $_POST["padding"]; ?> </td>
-              </tr>
-            </tbody>
-          </table>
+        <div class="wrapper">
+            <div class="flex-container">
+                <table>
+                    <tr>
+                        <th>key</th>
+                        <th>value</th>
+                    </tr>
+
+                    <?php
+                        $info2= array("naam"=>"naam", "achternaam"=>"achternaam", "klas"=>"klas", "muziek"=>"favorite music");
+                        $info= array("naam"=>"wouter", "achternaam"=>"Baas", "klas"=>"0D", "muziek"=>"Heavy metal");
+                        
+
+                        function maakrij($info2){
+
+                                return $info2;
+                        }
+                            foreach($info2 as $index => $thing){
+
+                                echo "<tr><td> $thing </td><td> $info[$index] </td></tr>";
+                            }
+
+                    ?>
+                    
+                </table>
+            </div>
+        </div>
     </body>
 </html>
